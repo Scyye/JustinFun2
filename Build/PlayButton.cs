267b5f18ae272b6ff10fb482dcaa5f82c97b07c7
Scyye <97131358+Scyye@users.noreply.github.com>
2023-04-22 22:34:13 -0400

    Add files via upload

---
+++ Build/PlayButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject playButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void OnClick() 
+    {
+        SceneManager.LoadScene("level 1");
+    }
+}
